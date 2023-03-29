d159c004737b8b21fcd62a43a6c83be4f150a918
Mytsum <alics2897@gmail.com>
2023-03-29 14:10:47 +0500

    add new app

---
+++ MyFirstAzureWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFirstAzureWebApp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+
+    private readonly IDictionary<string, string> Users = new Dictionary<string, string>()
+    {
+        { "test", "passcode"}
+    };
+
+    public string Authenticate(string userName, string password)
+    {
+        if (!Users.Any(t => t.Key == userName && t.Value == password))
+            return null;
+
+        return $"Login succeeded for user {userName}";
+    }
+
+    public int Add(int a, int b)
+    {
+        int c = a + b;
+        return c;
+    }
+
+
+}
