74b9c611dc57e2ada331f290ca5782decfe77a46
Mytsum <alics2897@gmail.com>
2023-04-25 15:33:46 +0500

    changes of tests

---
+++ SeleniumTestsProject/SeleniumTestOfGoogleSearch.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FirstAzureAppTestProject
+{
+    [TestClass()]
+    public class SeleniumTestOfGoogleSearch
+    {
+        [TestMethod]
+        public void Test_Search_Riphah_International_University()
+        {
+            int waitingTime = 2000;
+            By googleSearchBar = By.Name("q");
+            By googleSearchButton = By.Name("btnK");
+
+            IWebDriver webDriver = new ChromeDriver();
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.Navigate().GoToUrl("https://www.google.com.pk");
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.Manage().Window.Maximize();
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleSearchBar).SendKeys("Riphah International University");
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleSearchButton).Click();
+
+            webDriver.Quit();
+        }
+        [TestMethod]
+        public void Test_Search_Riphah_International_University_IsDisplayed()
+        {
+            string searchText = "riphah university";
+            string displayResult = "Riphah International University";
+            int waitingTime = 2000;
+            By googleSearchBar = By.Name("q");
+            By googleSearchButton = By.Name("btnK");
+            By googleResultText = By.XPath(".//h2//span[text()='" + displayResult + "']");
+
+            IWebDriver webDriver = new ChromeDriver();
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.Navigate().GoToUrl("https://www.google.com.pk");
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.Manage().Window.Maximize();
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleSearchBar).SendKeys(searchText);
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleSearchButton).Click();
+
+            Thread.Sleep(waitingTime);
+
+            var actualResultText = webDriver.FindElement(googleResultText);
+
+            Assert.IsTrue(actualResultText.Text.Equals(displayResult));
+
+            webDriver.Quit();
+        }
+        [TestMethod]
+        public void Test_Value_Check()
+        {
+            string searchText = "Selenium Test";
+            int waitingTime = 2000;
+            By textField = By.Name("inp");
+            By checkButton = By.Name("chk");
+            By outputField = By.Id("txtOutput");
+
+            IWebDriver webDriver = new ChromeDriver();
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.Navigate().GoToUrl("https://mywebappdevopssp23.azurewebsites.net/");
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.Manage().Window.Maximize();
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(textField).SendKeys(searchText);
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(checkButton).Click();
+
+            Thread.Sleep(waitingTime);
+
+            var actualResultText = webDriver.FindElement(outputField);
+
+            Assert.IsTrue(actualResultText.Text.Equals(searchText));
+
+            webDriver.Quit();
+        }
+    }
+}
